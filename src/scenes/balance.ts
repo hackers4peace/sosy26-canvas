54a424b20e54b9568c13ed3dcb06f396a7f7ba5c
Maciej Samoraj <maciej.samoraj@gmail.com>
2026-04-27 22:41:09 +0200

    Update balance.ts

--- src/scenes/balance.ts
+++ src/scenes/balance.ts
@@ -2,7 +2,7 @@ import { makeScene2D, Img } from "@motion-canvas/2d"
 import { beginSlide, all } from "@motion-canvas/core"
 export default makeScene2D(function* (view) {
   const seasaw = new Img({
-    src: 'https://i.imgur.com/j8sT8Qp.png',
+    src: 'https://raw.githubusercontent.com/hackers4peace/sosy26-canvas/refs/heads/main/src/j8sT8Qp.png',
     width: 1920,
     height: 1080,
   })
